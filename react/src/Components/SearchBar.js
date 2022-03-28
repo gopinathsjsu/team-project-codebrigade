57e108bc187095e6c9d2332ccc4f463fd4cd0bb1
Supreetha M A <supreetha@Supreethas-MacBook-Pro.local>
2022-03-28 16:46:01 -0700

    adding findButton and default values to fields in searchBar

--- react/src/Components/SearchBar.js
+++ react/src/Components/SearchBar.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Dropdown from 'react-bootstrap/Dropdown';
 import Form from "react-bootstrap/Form";
-import Dropdown from "react-bootstrap/Dropdown";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 import "./../Styles/searchBar.css";
+import "./../Assets/locIcon.png";
 
 const SearchBar = () => {
+
+    const [roomCount, setRoomCount] = useState(1);
+    const [adultCount, setAdultCount] = useState(1);
+    const [childCount, setChildCount] = useState(0);
+
+    const searchHotels = () => {
+        // call search API
+    };
+
+    const getDate = (day) => {
+        const today = new Date();
+        if (day === "today")
+            return today.toISOString().substr(0, 10);
+        const tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        return tomorrow.toISOString().substr(0, 10);
+    };
+
     return <span className="contents">
         <Form.Group className="searchAttribute">
             <Form.Label>Destination</Form.Label>
-            <Form.Control
-                type="text"
-                id="inputLocation"
-                placeholder="Where shall we go?"
-            />
+            <div className="searchDestination">
+                <Form.Control
+                    type="text"
+                    id="inputLocation"
+                    placeholder="Where shall we go?"
+                />
+                <FontAwesomeIcon icon={faLocationDot} />
+            </div>
         </Form.Group>
         <Form.Group className="searchAttribute">
             <Form.Label>Check In</Form.Label>
             <Form.Control
                 type="date"
                 id="checkIn"
-                placeholder="Check In"
+                placeholder="mm/dd/yyyy"
+                defaultValue={getDate("today")}
             />
         </Form.Group>
         <Form.Group className="searchAttribute">
@@ -26,22 +53,43 @@ const SearchBar = () => {
             <Form.Control
                 type="date"
                 id="CheckOut"
-                placeholder="Check Out"
+                placeholder="mm/dd/yyyy"
+                defaultValue={getDate("tommorow")}
             />
         </Form.Group>
-        <Dropdown>
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary" className="searchDropdown">
-                Rooms &amp; Guests
+        <Dropdown autoclose="outside">
+            <Dropdown.Toggle id="dropdown-autoclose-false" variant="secondary" className="searchDropdown">
+                {roomCount} Rooms &amp; {adultCount + childCount} Guests
             </Dropdown.Toggle>
 
             <Dropdown.Menu variant="dark">
-                <Dropdown.Item href="#/action-1" active>
+                <Dropdown.Item className="lisItem">
                     Rooms
+                    <span className="counter">
+                        <Button onClick={() => { if (roomCount > 1) setRoomCount(roomCount - 1) }}>-</Button>
+                        <span>{roomCount}</span>
+                        <Button onClick={() => setRoomCount(roomCount + 1)}>+</Button>
+                    </span>
                 </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Adults</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Children</Dropdown.Item>
+                <Dropdown.Item className="lisItem">
+                    Adults
+                    <span className="counter">
+                        <Button onClick={() => { if (adultCount > 1) setAdultCount(adultCount - 1) }}>-</Button>
+                        <span>{adultCount}</span>
+                        <Button onClick={() => setAdultCount(adultCount + 1)}>+</Button>
+                    </span>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Children
+                    <span className="counter">
+                        <Button onClick={() => { if (childCount > 0) setChildCount(childCount - 1) }}>-</Button>
+                        <span>{childCount}</span>
+                        <Button onClick={() => setChildCount(childCount + 1)}>+</Button>
+                    </span>
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
+        <Button className="findButton" onClick={searchHotels()} variant="primary">Find Hotels</Button>
     </span>
 }
 
