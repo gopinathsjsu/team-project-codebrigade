e65ce7f66c18c5310be05fd201dd8574285adbc3
Sarinder Virk <sarinder.virk@sjsu.edu>
2022-03-28 22:34:24 -0700

    wireframes for Booking UI

--- react/src/Components/Final.js
+++ react/src/Components/Final.js
@@ -1,32 +1,69 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 const Final = ({ values }) => {
 
-    //destructuring the object from values
+  //destructuring the object from values
   const { firstName, lastName, phoneNumber, creditCard, cvc } = values;
   return (
     <>
       <Card className="mt-3">
+      <Card.Header>Summary</Card.Header>
         <Card.Body>
-          <p>
-            <b>Booking confirmed!</b>
-          </p>
-          <p>
-            <strong>First Name :</strong> {firstName}{" "}
-          </p>
-          <p>
-            <strong>Last Name :</strong> {lastName}{" "}
-          </p>
-          <p>
-            <strong>Phone Number :</strong> {phoneNumber}{" "}
-          </p>
-          <p>
-            <strong>Credit Card :</strong> {creditCard}{" ***"}
-          </p>
-          <p>
-            <strong>CVC :</strong> {cvc}{" "}
-          </p>
+          <Container>
+            <Row>
+              <Col><Card.Title>Deluxe Queen, Guest room, 1 Queen, City view</Card.Title></Col>
+            </Row>
+            <Row>
+              <Col>
+                <Row>
+                  <Col>
+                    First Name
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Last Name
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Email@address.com
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                <Row>
+                  <Col>
+                    123 main st.
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Pleasanton
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    CA
+                  </Col>
+                  <Col>
+                    94566
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <hr />
+              <Row>
+                <Col>
+                  <Row>
+                    <Col>
+                      Credit Card: 4511 **** **** **** Exp: 4/5/22
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+          </Container>
         </Card.Body>
       </Card>
     </>
--- react/src/Components/StepTwo.js
+++ react/src/Components/StepTwo.js
@@ -1,184 +1,199 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Card, Button, Image, Accordion, Container, Row, Col } from "react-bootstrap";
+import validator from "validator";
 
 // creating functional component ans getting props from app.js and destucturing them
 const StepTwo = ({ nextStep, handleFormData, prevStep, values }) => {
+  //creating error state for validation
+  const [error, setError] = useState(false);
+
+  // after form submit validating the form data using validator
+  const submitFormData = (e) => {
+    e.preventDefault();
+
+    // checking if value of first name and last name is empty show error else take to next step
+    if (false) {//validator.isEmpty(values.cvc) || validator.isEmpty(values.creditCard)) {
+      setError(true);
+    } else {
+      nextStep();
+    }
+  };
 
   return (
     <Card className="mt-3">
-      <Card.Header>Reservation Details</Card.Header>
-      <Card.Body>
-        <Container>
-          <Row>
-            <Col><Card.Title>Deluxe Queen, Guest room, 1 Queen, City view</Card.Title></Col>
-          </Row>
-        </Container>
-        <Card.Text>
+      <Form onSubmit={submitFormData}>
+        <Card.Header>Reservation Details</Card.Header>
+        <Card.Body>
           <Container>
             <Row>
-              <Col><Image fluid={true} rounded={true} src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" /></Col>
-              <Col>
-                <Container>
-                  <Row>
-                    <Col>Check in: Friday, March 25, 2022</Col>
-                  </Row>
-                  <Row>
-                    <Col>Check out: Saturday, March 26, 2022</Col>
-                  </Row>
-                  <Row>
-                    <Col>Room(s): 1</Col>
-                  </Row>
-                  <Row>
-                    <Col>Guest(s) per room: 1</Col>
-                  </Row>
-                </Container>
-              </Col>
-            </Row>
-            <Row className="mt-3">
-              <Accordion alwaysOpen>
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header>Choose Room Options</Accordion.Header>
-                  <Accordion.Body>
-                    <Container>
-                      <Row>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Late check-in"
-                          /></Col>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Near elevator"
-                          /></Col>
-
-                      </Row>
-                      <Row>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Extra pillows"
-                          /></Col>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Rollaway bed"
-                          /></Col>
-
-                      </Row>
-                      <Row>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Crib"
-                          /></Col>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Waterbed"
-                          /></Col>
-
-                      </Row>
-                      <Row>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Extra towels"
-                          /></Col>
-                        <Col>
-                          <Form.Check
-                            type="checkbox"
-                            id="waterbed"
-                            label="Hairdryer"
-                          /></Col>
-                      </Row>
-                    </Container>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                  <Accordion.Header>Summary of Charges</Accordion.Header>
-                  <Accordion.Body>
-                    <Container>
-                      <Row>
-                        <Col>
-                          <b>Flex rate</b>
-                        </Col>
-                        <Col>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          <i>1 room for 1 night</i>
-                        </Col>
-                        <Col>
-                          Prices in USD
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          <hr />
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          Saturday March 26, 2022
-                        </Col>
-                        <Col>
-                          354.23
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          Taxes and fees
-                        </Col>
-                        <Col>
-                          78.90
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          <hr />
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col>
-                          <b>Total</b>
-                        </Col>
-                        <Col>
-                          <b>454.23</b>
-                        </Col>
-                      </Row>
-                    </Container>
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
+              <Col><Card.Title>Deluxe Queen, Guest room, 1 Queen, City view</Card.Title></Col>
             </Row>
           </Container>
-        </Card.Text>
-      </Card.Body>
-      <Container className="mb-3">
-        <Row className="text-end">
-          <Col>
-            <Button variant="secondary" onClick={prevStep}>
-              Previous
-            </Button>
-          </Col>
-          <Col>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+          <Card.Text>
+            <Container>
+              <Row>
+                <Col><Image fluid={true} rounded={true} src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" /></Col>
+                <Col>
+                  <Container>
+                    <Row>
+                      <Col>Check in: Friday, March 25, 2022</Col>
+                    </Row>
+                    <Row>
+                      <Col>Check out: Saturday, March 26, 2022</Col>
+                    </Row>
+                    <Row>
+                      <Col>Room(s): 1</Col>
+                    </Row>
+                    <Row>
+                      <Col>Guest(s) per room: 1</Col>
+                    </Row>
+                  </Container>
+                </Col>
+              </Row>
+              <Row className="mt-3">
+                <Accordion alwaysOpen>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>Choose Room Options</Accordion.Header>
+                    <Accordion.Body>
+                      <Container>
+                        <Row>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Late check-in"
+                            /></Col>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Near elevator"
+                            /></Col>
+
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Extra pillows"
+                            /></Col>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Rollaway bed"
+                            /></Col>
+
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Crib"
+                            /></Col>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Waterbed"
+                            /></Col>
+
+                        </Row>
+                        <Row>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Extra towels"
+                            /></Col>
+                          <Col>
+                            <Form.Check
+                              type="checkbox"
+                              id="waterbed"
+                              label="Hairdryer"
+                            /></Col>
+                        </Row>
+                      </Container>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>Summary of Charges</Accordion.Header>
+                    <Accordion.Body>
+                      <Container>
+                        <Row>
+                          <Col>
+                            <b>Flex rate</b>
+                          </Col>
+                          <Col>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <i>1 room for 1 night</i>
+                          </Col>
+                          <Col>
+                            Prices in USD
+                          </Col>
+                        </Row>
+                        <Row className="border-bottom pt-1">
+                          <Col>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            Saturday March 26, 2022
+                          </Col>
+                          <Col>
+                            354.23
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            Taxes and fees
+                          </Col>
+                          <Col>
+                            78.90
+                          </Col>
+                        </Row>
+                        <Row className="border-bottom pt-1">
+                          <Col>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                            <b>Total</b>
+                          </Col>
+                          <Col>
+                            <b>454.23</b>
+                          </Col>
+                        </Row>
+                      </Container>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Row>
+            </Container>
+          </Card.Text>
+        </Card.Body>
+        <Container className="mb-3">
+          <Row className="text-end">
+            <Col></Col>
+            <Col>
+              <Button variant="secondary" onClick={prevStep}>
+                Previous
+              </Button>
+            </Col>
+            <Col className="text-end">
+              <Button variant="primary" onClick={() => nextStep()}>Continue</Button>
+            </Col>
+          </Row>
+        </Container>
+      </Form>
     </Card>
   );
+
 };
 
 export default StepTwo;
