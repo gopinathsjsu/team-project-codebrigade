d951c53e6d50d73d2c4a48d4bc4b59565a88ccad
Supreetha M A <supreethama@gmail.com>
2022-04-07 16:24:16 -0700

    removed duplicate SignUp.js file and added styling to SignUp page

--- react/src/Pages/Signup.js
+++ react/src/Pages/Signup.js
@@ -1,19 +1,19 @@
 //import { CognitoAccessToken, CognitoUserPool } from "amazon-cognito-identity-js";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import UserPool from "../UserPool";
-import {useNavigate} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "../Styles/SignUp.css";
 
 
 const Signup = () => {
-    const [email, setEmail]= useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("");
-    const [given_name, set_given_name]=  useState("");
-    const [family_name, set_family_name] =  useState("");
-    const [gender, setGender ]=  useState("");
-    const [birthdate, setBirthdate] =  useState("");
+    const [given_name, set_given_name] = useState("");
+    const [family_name, set_family_name] = useState("");
+    const [gender, setGender] = useState("");
+    const [birthdate, setBirthdate] = useState("");
 
-    const onSubmit =(event) => {
+    const onSubmit = (event) => {
         event.preventDefault();
 
         UserPool.signUp(email, password, given_name, family_name, birthdate, gender, [], null, (err, data) => {
@@ -21,47 +21,76 @@ const Signup = () => {
                 console.log(err);
             }
             console.log(data)
-         });
+        });
     };
-    const navigate= useNavigate();
-    return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                    value={email}
-                    onChange={(event)=> setEmail(event.target.value)}
-                    ></input>
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(event)=> setPassword(event.target.value)}
-                    ></input>
-                <label htmlFor="given_name">First Name</label>
-                <input
-                    value={given_name}
-                    onChange={(event)=> set_given_name(event.target.value)}
-                    ></input>
-                <label htmlFor="family_name">Last Name</label>
-                <input
-                    value={family_name}
-                    onChange={(event)=> set_family_name(event.target.value)}
-                    ></input>
-                <label htmlFor="birthdate">Birthdate</label>
-                <input
-                    value={birthdate}
-                    onChange={(event)=> setBirthdate(event.target.value)}
-                    ></input>
-                <label htmlFor="gender">Gender</label>
-                <input
-                    value={gender}
-                    onChange={(event)=> setGender(event.target.value)}
-                    ></input>
-                <button type="submit">Signup</button>
-                <button onClick={() => {navigate("/Login")}}>Login?Already a user</button>
-                </form>
 
+    return (
+        <div className="Contents">
+            <header className="title">Create an account</header>
+            <div className="fields-container">
+                <form onSubmit={onSubmit}>
+                    <div className="field-container">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            className="field"
+                            value={email}
+                            placeholder="email@domain.com"
+                            onChange={(event) => setEmail(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            className="field"
+                            type="password"
+                            value={password}
+                            placeholder="password"
+                            onChange={(event) => setPassword(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="given_name">First Name</label>
+                        <input
+                            className="field"
+                            value={given_name}
+                            placeholder="Given name"
+                            onChange={(event) => set_given_name(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="family_name">Last Name</label>
+                        <input
+                            className="field"
+                            value={family_name}
+                            placeholder="Family name"
+                            onChange={(event) => set_family_name(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="birthdate">Birthdate</label>
+                        <input
+                            className="field"
+                            value={birthdate}
+                            placeholder="mm-dd-yyy"
+                            onChange={(event) => setBirthdate(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="gender">Gender</label>
+                        <input
+                            className="field"
+                            value={gender}
+                            placeholder="Male/female"
+                            onChange={(event) => setGender(event.target.value)}
+                        ></input>
+                    </div>
+                    <div className="clikables">
+                        <span className="hint-text">Have an account? </span>
+                        <Link className="Sign-In-link" to="/Login">SignIn</Link>
+                        <button className="create-button" type="submit">Create account</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
