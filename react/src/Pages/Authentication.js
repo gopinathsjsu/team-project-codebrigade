4c6b0657c9bfff4527c804055bb30c4e3a68264f
Supreetha M A <supreethama@gmail.com>
2022-05-05 11:30:43 -0700

    SignIn choice page UI modifications

--- react/src/Pages/Authentication.js
+++ react/src/Pages/Authentication.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import Button from "react-bootstrap/Button";
 import "./../Styles/searchBar.css";
+import { Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 
 
 const Authentication = () => {
+  const navigate = useNavigate();
+  return <>
+    <Header />
+    <div className="Contents">
+      <div className="fields-container">
+        <Row>
+          <h2 className="title">Who you are?</h2>
+        </Row>
+        <Row className="margintop10">
+          <Col>
+            <Button onClick={() => navigate("/SignUpAdmin")}>SignIn as Admin</Button>
+          </Col>
+          <Col>
+            <Button onClick={() => navigate("/SignUp")}>SignIn as User</Button>
+          </Col>
+        </Row>
+      </div>
+    </div>
 
-    return <>
-  
-      <Header/>
-      <Link to="/SignUpAdmin">SignIn as Admin</Link><br/>
-      <Link to="/SignUp">SignIn as User</Link>
+  </>;
+};
 
-    </>;
-  };
-  
-  export default Authentication;
-  
+export default Authentication;
