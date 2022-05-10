9c0039aa12b7055a3e3e25cdefd8cb61532fc207
Supreetha M A <supreethama@gmail.com>
2022-05-10 11:49:38 -0700

    removing custome list link in header

--- react/src/Components/Header.js
+++ react/src/Components/Header.js
@@ -11,7 +11,6 @@ const Header = () => {
       {isUserLoggedIn ? <Link className="link" to="/myRewards">My Rewards</Link> : ""}
       {!isUserLoggedIn ? <Link className="link" to="/signInOrJoin">SignIn or Join</Link> : ""}
       {isUserLoggedIn ? <Link className="link" to="/myTrips">My Trips</Link> : ""}
-      <Link className="link" to="/customerList">Customer List</Link>
       <div></div>
     </header>
   </>
